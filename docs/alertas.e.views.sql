0711e00337475ac82e185ca3d21ba12085373d29
Zabriduel <gabriel31alves@gmail.com>
2026-05-20 11:16:33 -0300

    feat: atualizar estoque

--- docs/alertas.e.views.sql
+++ docs/alertas.e.views.sql
@@ -1,30 +1,30 @@
-SELECT * FROM lotes WHERE qtd_lote <= 20;
-SELECT * FROM lotes WHERE DATEDIFF(data_vencimento, CURDATE())  = 90;
-SELECT * FROM lotes WHERE DATEDIFF(data_vencimento, CURDATE())  = 45;
+	SELECT * FROM lotes WHERE qtd_lote <= 20;
+	SELECT * FROM lotes WHERE DATEDIFF(data_vencimento, CURDATE())  = 90;
+	SELECT * FROM lotes WHERE DATEDIFF(data_vencimento, CURDATE())  = 45;
 
-CREATE VIEW vw_vencimento_90_dias AS 
-    SELECT * FROM lotes WHERE data_vencimento = DATE_ADD(CURDATE(), INTERVAL 90 DAY); 
+	CREATE VIEW vw_vencimento_90_dias AS 
+		SELECT * FROM lotes WHERE data_vencimento = DATE_ADD(CURDATE(), INTERVAL 90 DAY); 
 
-CREATE VIEW vw_vencimento_45_dias AS 
-    SELECT * FROM lotes WHERE data_vencimento = DATE_ADD(CURDATE(), INTERVAL 45 DAY);
+	CREATE VIEW vw_vencimento_45_dias AS 
+		SELECT * FROM lotes WHERE data_vencimento = DATE_ADD(CURDATE(), INTERVAL 45 DAY);
 
-CREATE VIEW vw_lote_baixo AS 
-    SELECT * FROM lotes WHERE qtd_lote <= 20;
-
-
-CREATE VIEW vw_relatorio_produtos_estoque AS
-SELECT
-    p.id_produto AS codigo_produto,
-    p.nome_produto AS produto,
-    SUM(l.qtd_lote) AS estoque_total
-FROM produtos p
-INNER JOIN lotes l 
-    ON l.fk_id_produto = p.id_produto
-GROUP BY p.id_produto, p.nome_produto;
+	CREATE VIEW vw_lote_baixo AS 
+		SELECT * FROM lotes WHERE qtd_lote <= 20;
 
 
-SELECT * FROM vw_relatorio_produtos_estoque;
-SELECT * FROM vw_vencimento_45_dias;
-SELECT * FROM vw_vencimento_90_dias;
-SELECT * FROM vw_lote_baixo;
+	CREATE VIEW vw_relatorio_produtos_estoque AS
+	SELECT
+		p.id_produto AS codigo_produto,
+		p.nome_produto AS produto,
+		SUM(l.qtd_lote) AS estoque_total
+	FROM produtos p
+	INNER JOIN lotes l 
+		ON l.fk_id_produto = p.id_produto
+	GROUP BY p.id_produto, p.nome_produto;
+
+
+	SELECT * FROM vw_relatorio_produtos_estoque;
+	SELECT * FROM vw_vencimento_45_dias;
+	SELECT * FROM vw_vencimento_90_dias;
+	SELECT * FROM vw_lote_baixo;
 
